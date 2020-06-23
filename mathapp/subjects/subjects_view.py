555acdcdf2455f595c6d98857d6bcc361bf8e306
Peter Cerhan <peter@foliahealth.com>
2020-06-22 21:39:59 -0400

    Organize subject view and controller

--- mathapp/subjects/subjects_view.py
+++ mathapp/subjects/subjects_view.py
@@ -1,93 +1,39 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-from werkzeug.exceptions import abort
-
 from mathapp.auth import login_required
-from mathapp.db import get_db
-
 from mathapp.subjects.subjects_web_controller import SubjectsWebController
 
 bp = Blueprint('subjects', __name__)
 
+## Index
+
 @bp.route('/')
 def index():
-    db = get_db()
-    subjects = db.execute(
-        'SELECT id, name'
-        ' FROM subject'
-    ).fetchall()
-    return render_template('subjects/index.html', subjects=subjects)
+    controller = SubjectsWebController(request)
+    return controller.handle_index_request()
+
+## Create
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
-    controller = SubjectsWebController()
-    return controller.handleCreateRequest(request)
+    controller = SubjectsWebController(request)
+    return controller.handle_create_request()
 
-
-#    if request.method == 'POST':
-#        name = request.form['name']
-#        error = None
-#
-#        if not name:
-#            error = 'Name is required.'
-#
-#        if error is not None:
-#            flash(error)
-#        else:
-#            db = get_db()
-#            db.execute(
-#                'INSERT INTO subject (name)'
-#                ' VALUES (?)',
-#                (name,)
-#            )
-#            db.commit()
-#            return redirect(url_for('subjects.index'))
-#
-#    return render_template('subjects/create.html')
-
-
-def get_subject(id):
-    subject = get_db().execute('SELECT id, name FROM subject WHERE id = ?', (id,)).fetchone()
-
-    if subject is None:
-        abort(404, "Subject id {0} doesn't exist.".format(id))
-
-    return subject
+## Update
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    subject = get_subject(id)
+    controller = SubjectsWebController(request)
+    return controller.handle_update_request(id)
 
-    if request.method == 'POST':
-        name = request.form['name']
-        error = None
-        
-        if not name:
-            error = 'Name is required.'
-        
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE subject SET name = ?'
-                ' Where id = ?',
-                (name, id)
-            )
-            db.commit()
-            return redirect(url_for('subjects.index'))
-    
-    return render_template('subjects/update.html', subject=subject)
-
+## Delete
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_subject(id)
-    db = get_db()
-    db.execute('DELETE FROM subject WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('subjects.index'))
+    controller = SubjectsWebController(request)
+    return controller.handle_delete_request(id)
+
--- mathapp/subjects/subjects_web_controller.py
+++ mathapp/subjects/subjects_web_controller.py
@@ -1,33 +1,84 @@
 from flask import (
-    request, flash, redirect, url_for, render_template
+    request, flash, redirect, url_for, render_template, abort
 )
 from mathapp.db import get_db
 
 class SubjectsWebController:
     
-    def __init__(self):
-        pass
+    def __init__(self, request):
+        self.request = request
         
-    def handleCreateRequest(self, request):
-        if request.method == 'POST':
-            name = request.form['name']
-            error = None
-
-            if not name:
-                error = 'Name is required.'
-
-            if error is not None:
-                flash(error)
-            else:
-                db = get_db()
-                db.execute(
-                    'INSERT INTO subject (name)'
-                    ' VALUES (?)',
-                    (name,)
-                )
-                db.commit()
-                return redirect(url_for('subjects.index'))
-
-        return render_template('subjects/create.html')
+    def handle_index_request(self):
+        db = get_db()
+        subjects = db.execute(
+            'SELECT id, name'
+            ' FROM subject'
+        ).fetchall()
+        return render_template('subjects/index.html', subjects=subjects)
+        
+    def handle_create_request(self):
+        if self.request.method == 'POST':
+            return self._post_create_form()
+        else:
+            return render_template('subjects/create.html')
     
+    def _post_create_form(self):
+        name = self.request.form['name']
+        error = None
 
+        if not name:
+            error = 'Name is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO subject (name)'
+                ' VALUES (?)',
+                (name,)
+            )
+            db.commit()
+            return redirect(url_for('subjects.index'))
+
+    def handle_update_request(self, id):
+        if self.request.method == 'POST':
+            return self._post_update_form(id)
+        else:
+            return render_template('subjects/update.html', subject=self._get_subject(id))
+
+    def _post_update_form(self, id):
+        subject = self._get_subject(id)
+        name = self.request.form['name']
+        error = None
+        
+        if not name:
+            error = 'Name is required.'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE subject SET name = ?'
+                ' Where id = ?',
+                (name, id)
+            )
+            db.commit()
+            return redirect(url_for('subjects.index'))
+    
+        
+    def _get_subject(self, id):
+        subject = get_db().execute('SELECT id, name FROM subject WHERE id = ?', (id,)).fetchone()
+
+        if subject is None:
+            abort(404, "Subject id {0} doesn't exist.".format(id))
+
+        return subject
+
+    def handle_delete_request(self, id):
+        self._get_subject(id)
+        db = get_db()
+        db.execute('DELETE FROM subject WHERE id = ?', (id,))
+        db.commit()
+        return redirect(url_for('subjects.index'))
