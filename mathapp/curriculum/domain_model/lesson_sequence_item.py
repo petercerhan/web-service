539e328eedafaeca38ba40fc4083b02352f9ccfa
Peter Cerhan <peter@foliahealth.com>
2020-07-30 18:44:22 -0400

    Display lessons associated with course in web interface

--- mathapp/curriculum/domain_model/lesson_sequence_item.py
+++ mathapp/curriculum/domain_model/lesson_sequence_item.py
@@ -11,5 +11,8 @@ class LessonSequenceItem:
 
 		self._unit_of_work = unit_of_work
 
+	def get_lesson(self):
+		return self._lesson
+
 	def __repr__(self):
 		return "<LessonSequenceItem(position='%s') lesson(name='%s')>" % (self._position, self._lesson.get_name())
