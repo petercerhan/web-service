1ea7ebcfed750109bed687fd204516600151c6f3
Peter Cerhan <peter@foliahealth.com>
2020-08-25 21:31:02 -0400

    System composer

--- mathapp/flask/root_composer.py
+++ mathapp/flask/root_composer.py
@@ -1,15 +1,7 @@
 from mathapp.curriculum.curriculum_composer import CurriculumComposer
-
+from mathapp.system.system_composer import SystemComposer
 
 from mathapp.sqlalchemy.unit_of_work import UnitOfWork
-
-
-
-from mathapp.system.controller.auth_web_controller import AuthWebController
-from mathapp.system.presenter.auth_presenter import AuthPresenter
-from mathapp.system.interactor.auth_interactor import AuthInteractor
-from mathapp.system.data_mapper.user.user_repository import UserRepository
-from mathapp.system.data_mapper.user.user_factory import UserFactory
 
 from mathapp.flask.flask_session import FlaskSession
 
@@ -25,6 +17,15 @@ class RootComposer:
 
         self._unit_of_work = None
         self._user_repository = None
+
+
+    def compose_unit_of_work(self):
+        if self._unit_of_work is None:
+            self._unit_of_work = UnitOfWork(self._session)
+            return self._unit_of_work
+        else:
+            return self._unit_of_work
+
 
     def compose_course_web_controller(self):
         unit_of_work = self.compose_unit_of_work()
@@ -42,51 +43,17 @@ class RootComposer:
 
         return curriculum_composer.compose_lesson_web_controller()
 
-
-
-    def compose_unit_of_work(self):
-        if self._unit_of_work is None:
-            self._unit_of_work = UnitOfWork(self._session)
-            return self._unit_of_work
-        else:
-            return self._unit_of_work
-
-
     def compose_auth_web_controller(self):
+        unit_of_work = self.compose_unit_of_work()
         flask_session = self.compose_flask_session()
-        presenter = self.compose_auth_presenter()
-        interactor = self.compose_auth_interactor()
-        return AuthWebController(request = self._request, 
-                                 flask_session = flask_session,
-                                 auth_presenter = presenter, 
-                                 auth_interactor = interactor)
-
-    def compose_auth_presenter(self):
-        return AuthPresenter()
-
-    def compose_auth_interactor(self):
-        unit_of_work = self.compose_unit_of_work()
-        user_repository = self.compose_user_repository()        
-        user_factory = self.compose_user_factory()
         encryption_service = self.compose_encryption_service()
-        return AuthInteractor(user_repository = user_repository, 
-                                user_factory = user_factory,
-                                encryption_service = encryption_service,
-                                unit_of_work_committer = unit_of_work)
+        system_composer = SystemComposer(request = self._request, 
+                                        sqlalchemy_session = self._session, 
+                                        unit_of_work = self._unit_of_work, 
+                                        flask_session = flask_session, 
+                                        encryption_service = encryption_service)
 
-    def compose_user_factory(self):
-        unit_of_work = self.compose_unit_of_work()
-        user_repository = self.compose_user_repository()
-        return UserFactory(unit_of_work = unit_of_work, user_repository = user_repository)
-
-    def compose_user_repository(self):
-        if self._user_repository is not None:
-            return self._user_repository
-        unit_of_work = self.compose_unit_of_work()
-        user_repository = UserRepository(unit_of_work = unit_of_work, 
-                                            session = self._session)
-        self._user_repository = user_repository
-        return user_repository
+        return system_composer.compose_auth_web_controller()
 
     def compose_flask_session(self):
         return FlaskSession()
---
+++ mathapp/system/system_composer.py
@@ -0,0 +1,61 @@
+from mathapp.system.controller.auth_web_controller import AuthWebController
+from mathapp.system.presenter.auth_presenter import AuthPresenter
+from mathapp.system.interactor.auth_interactor import AuthInteractor
+from mathapp.system.data_mapper.user.user_repository import UserRepository
+from mathapp.system.data_mapper.user.user_factory import UserFactory
+
+class SystemComposer:
+
+    def __init__(self, 
+                request, 
+                sqlalchemy_session, 
+                unit_of_work, 
+                flask_session, 
+                encryption_service):
+        self._request = request
+        self._sqlalchemy_session = sqlalchemy_session
+        self._unit_of_work = unit_of_work
+        self._flask_session = flask_session
+        self._encryption_service = encryption_service
+
+        ##Singleton Lifestyle Components
+
+        self._user_repository = None
+
+
+    ##Auth Web Controller
+
+    def compose_auth_web_controller(self):
+        presenter = self.compose_auth_presenter()
+        interactor = self.compose_auth_interactor()
+        return AuthWebController(request = self._request, 
+                                 flask_session = self._flask_session,
+                                 auth_presenter = presenter, 
+                                 auth_interactor = interactor)
+
+    def compose_auth_presenter(self):
+        return AuthPresenter()
+
+    def compose_auth_interactor(self):
+        user_repository = self.compose_user_repository()        
+        user_factory = self.compose_user_factory()
+        return AuthInteractor(user_repository = user_repository, 
+                                user_factory = user_factory,
+                                encryption_service = self._encryption_service,
+                                unit_of_work_committer = self._unit_of_work)
+
+    def compose_user_factory(self):
+        user_repository = self.compose_user_repository()
+        return UserFactory(unit_of_work = self._unit_of_work, user_repository = user_repository)
+
+    def compose_user_repository(self):
+        if self._user_repository is not None:
+            return self._user_repository
+        user_repository = UserRepository(unit_of_work = self._unit_of_work, 
+                                            session = self._sqlalchemy_session)
+        self._user_repository = user_repository
+        return user_repository
+
+
+
+
