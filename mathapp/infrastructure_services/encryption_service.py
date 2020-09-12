9c92994898b48ac7f2bb54b1a987e4a91d91c66f
Peter Cerhan <peter@foliahealth.com>
2020-09-12 17:58:37 -0400

    Extract csrf hash key to config file

--- mathapp/infrastructure_services/encryption_service.py
+++ mathapp/infrastructure_services/encryption_service.py
@@ -8,6 +8,9 @@ import sys
 
 class EncryptionService:
 
+	def __init__(self, csrf_hash_key):
+		self._csrf_hash_key = csrf_hash_key.encode('utf-8')
+
 	def generate_password_hash(self, password):
 		return generate_password_hash(password)
 
@@ -15,15 +18,13 @@ class EncryptionService:
 		return check_password_hash(encrypted_password, check_password)
 
 	def generate_hash_for_csrf(self, message):
-		secret = b'test_secret'
 		encoded_message = message.encode('utf-8')
-		message_hash = base64.b64encode(hmac.new(secret, encoded_message, digestmod=hashlib.sha256).digest())
+		message_hash = base64.b64encode(hmac.new(self._csrf_hash_key, encoded_message, digestmod=hashlib.sha256).digest())
 		return message_hash.decode('utf-8')
 
 	def check_hash_for_csrf(self, check_hash, message):
-		secret = b'test_secret'
 		encoded_check_hash = check_hash.encode('utf-8')
 		encoded_message = message.encode('utf-8')
-		message_hash = base64.b64encode(hmac.new(secret, encoded_message, digestmod=hashlib.sha256).digest())
+		message_hash = base64.b64encode(hmac.new(self._csrf_hash_key, encoded_message, digestmod=hashlib.sha256).digest())
 		return hmac.compare_digest(message_hash, encoded_check_hash)
 
--- mathapp/infrastructure_services/infrastructure_service_composer.py
+++ mathapp/infrastructure_services/infrastructure_service_composer.py
@@ -7,11 +7,12 @@ from flask import current_app
 class InfrastructureServiceComposer:
     
     def compose_encryption_service(self):
-        return EncryptionService()
+        csrf_hash_key = current_app.config['CSRF_HASH_KEY']
+        return EncryptionService(csrf_hash_key=csrf_hash_key)
 
     def compose_token_service(self):
-    	web_signing_key = current_app.config['AUTH_SECRET_KEY']
-    	return TokenService(web_signing_key=web_signing_key)
+        web_signing_key = current_app.config['AUTH_SECRET_KEY']
+        return TokenService(web_signing_key=web_signing_key)
 
     def compose_date_service(self):
-    	return DateService()
+        return DateService()
