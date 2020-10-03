bc896266dc9d1d37f53148c9c127e8d760219e50
Peter Cerhan <peter@foliahealth.com>
2020-10-03 17:12:45 -0400

    Update lessons index and update tests to use calculated urls

--- tests/lessons/test_lessons_index.py
+++ tests/lessons/test_lessons_index.py
@@ -1,13 +1,14 @@
 import pytest
 from mathapp.curriculum.data_mapper.lesson.orm_lesson import ORMLesson
+from flask import url_for
 
 
 def test_get_lessons_index(client, auth):
 	auth.login()
-	response = client.get('/lessons/')
+	response = client.get(url_for('lessons.index'))
 	assert response.status_code == 200
 
 def test_get_lessons_index_not_authenticated(client):
-	response = client.get('/lessons/')
+	response = client.get(url_for('lessons.index'))
 	assert response.status_code == 302
 	assert response.headers.get('Location') == 'http://localhost/auth/login'
