fb6447b847b7da09b7ebe525bb2a9f8b776bbdfa
Peter Cerhan <peter@foliahealth.com>
2020-10-15 21:27:50 -0400

    Get branch for detail section

--- mathapp/curriculum/domain_model/node_content.py
+++ mathapp/curriculum/domain_model/node_content.py
@@ -5,3 +5,6 @@ class NodeContent:
 	def __init__(self, model, model_name):
 		self.model = model
 		self.model_name = model_name
+
+	def __repr__(self):
+		return f'NodeContent for model: {self.model}'
--- mathapp/curriculum/interactor/detail_section_interactor.py
+++ mathapp/curriculum/interactor/detail_section_interactor.py
@@ -1,4 +1,5 @@
 from mathapp.curriculum.interactor.domain_to_data_transforms.detail_section import detail_section_to_data
+from mathapp.curriculum.domain_model.node_content import NodeContent
 
 import sys
 
@@ -21,3 +22,16 @@ class DetailSectionInteractor:
 
 		self._unit_of_work.commit()
 
+	def get_branch_for_node(self, id):
+		detail_section = self._detail_section_repository.get(id)
+		detail_section_node = NodeContent(model=detail_section, model_name='detail_section')
+		nodes = [detail_section_node]
+		node = detail_section_node
+		while self.responds_to_get_parent(node.model):
+			node = node.model.get_parent()
+			nodes.insert(0, node)
+
+		##flatten nodes to data somehow
+
+	def responds_to_get_parent(self, model):
+		return (hasattr(model, 'get_parent') and callable(model.get_parent))
