2f002c911c005d8292fb23ff5d3ac8aeb77b3a42
Peter Cerhan <peter@foliahealth.com>
2021-09-12 16:05:52 -0400

    Link from edit problem set generator back to edit lesson

--- mathapp/curriculum/interactor/domain_to_data_transforms/lesson.py
+++ mathapp/curriculum/interactor/domain_to_data_transforms/lesson.py
@@ -1,6 +1,7 @@
 
 
 def lesson_to_data(lesson):
+	
 	return {'id': lesson.get_id(),
 			'name': lesson.get_name(),
 			'position': lesson.get_position()}
--- mathapp/curriculum/interactor/domain_to_data_transforms/list_problem_set_generator.py
+++ mathapp/curriculum/interactor/domain_to_data_transforms/list_problem_set_generator.py
@@ -1,7 +1,9 @@
-
+from mathapp.curriculum.interactor.domain_to_data_transforms.lesson import lesson_to_data
 
 def list_problem_set_generator_to_data(list_problem_set_generator):
+	lesson = lesson_to_data(list_problem_set_generator.get_lesson())
 	return {'id': list_problem_set_generator.get_id(), 
 			'type': list_problem_set_generator.get_type(), 
-			'name': list_problem_set_generator.get_name()}
+			'name': list_problem_set_generator.get_name(),
+			'lesson': lesson}
 
